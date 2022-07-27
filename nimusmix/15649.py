a59ea1ad423d50a308f2cfd539cb99c91d0b270d
nimusmix <nimusmix@gmail.com>
2022-07-27 23:56:33 +0900

    [BOJ] N과 M (1) / 실버3 / 168ms

--- nimusmix/15649.py
+++ nimusmix/15649.py
@@ -1,7 +1,6 @@
 from itertools import permutations
-import itertools
 
 a, b = map(int, input().split())
 
-for i in itertools.permutations(range(1,a+1), b):
+for i in permutations(range(1,a+1), b):
     print(*i)
