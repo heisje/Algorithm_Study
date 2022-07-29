1d1373e29ba7e2b2a3361acd0fe2bd047dff1624
Sumin Kim <109320569+nimusmix@users.noreply.github.com>
2022-07-29 22:14:45 +0900

    Delete 2605.py

--- nimusmix/2605.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-m = list(map(int, input().split()))
-line = []
-
-# 번호표를 뽑은 학생이 들어가는 자리는 본인 위치에서 본인이 뽑은 번호를 뺀 자리와 같음.
-for i in range(n):
-    line.insert(i-m[i], i+1)
-
-print(*line)
