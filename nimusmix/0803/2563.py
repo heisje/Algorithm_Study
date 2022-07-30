c0a8aafa8b8227baec3739f588e111f41f3dad54
nimusmix <nimusmix@gmail.com>
2022-07-30 22:44:19 +0900

    [BOJ] 색종이 / 브론즈1 / 80ms: .w d/

--- nimusmix/0803/2563.py
+++ nimusmix/0803/2563.py
@@ -1,15 +1,15 @@
 n = int(input())
-paper = [[0] * 100 for _ in range(100)]    # 전체 도화지 크기의 이차원 리스트 생성
+paper = [[0] * 100 for _ in range(100)]       # 전체 도화지 크기의 이차원 리스트 생성
 
 for _ in range(n):
     a, b = map(int, input().split())
     
-    for i in range(b, b+10):               # 입력 받은 가로와 세로 만큼의 범위를 1로 변경
+    for i in range(b, b+10):                  # 입력 받은 가로와 세로 만큼의 범위를 1로 변경
         for j in range(a, a+10):
             paper[i][j] = 1
             
 cnt = 0
-for i in paper:                            # 전체 1의 합계 출력
+for i in paper:                               # 전체 1의 합계 출력
     cnt += sum(i)
             
 print(cnt)
