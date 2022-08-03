7db92c038ce5fd07bb3fb5bd2a569c375abca995
bbookng <bbookng@gmail.com>
2022-08-03 15:39:56 +0900

    [BOJ] 줄세우기 / 브론즈3 / 68ms

---
+++ bbookng/0803/13300_방 배정.py
@@ -0,0 +1,21 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+n, k = map(int, input().split())
+
+cnt = 0
+arr = [[0] * 6 for _ in range(2)]                   # 남,녀 1~6학년 2중 list 생성
+
+for _ in range(n):
+    x, y = map(int,input().split())
+    arr[x][y-1] += 1                                # x, y값에 따라 리스트의 각 항목에 +1
+
+result = 0
+for i in range(2):                                  # 성별을 기준으로 순환
+    for j in range(6):                              # 학년을 기준으로 순환
+        
+         # i성별 j학년의 학생 수에서 한 방에 배정할 수 있는 인원 수인 k 나누기
+         # arr[i][j]가 1이상인 경우, k의 배수가 아닌 경우 그냥 몫보다 방이 1개 더 필요 하므로 k-1을 더해서 k로 나눔
+        result += (arr[i][j] + k - 1) // k         
+print(result)
---
+++ bbookng/0803/2563_색종이.py
@@ -0,0 +1,21 @@
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input())
+
+board = [[0] * 100 for _ in range(100)]         # 100x100 도화지 생성
+
+for _ in range(n):
+    i, j = map(int,input().split())             
+    for x in range(i, i+10):                    # 입력받은 수 기준으로 변의 길이 10 만큼
+        for y in range(j, j+10):                # board에 해당 넓이 만큼 1로 만들어줌.
+            board[x][y] = 1
+
+result = 0
+
+for i in range(len(board)):                     # 1인 항목들의 갯수만큼 result에 저장
+    result += sum(board[i])
+
+print(result)
+
