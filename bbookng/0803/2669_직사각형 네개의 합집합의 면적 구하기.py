5e757e0653f25d60f72c87a932bb8c94471f8213
bbookng <bbookng@gmail.com>
2022-08-03 15:44:17 +0900

    [BOJ] 직사각형 네개~ / 브론즈1 / 68ms

--- bbookng/0803/2669_직사각형 네개의 합집합의 면적 구하기.py
+++ bbookng/0803/2669_직사각형 네개의 합집합의 면적 구하기.py
@@ -6,8 +6,8 @@ board = [[0] * 100 for _ in range(100)]
 
 for _ in range(4):
     x1, y1, x2, y2 = map(int,input().split())
-    for x in range(x1, x2):                     # 직사각형의 가로길이만큼
-        for y in range(y1, y2):                 # 직사각형의 세로길이만큼
+    for x in range(x1, x2):                     # 직사각형의 가로길이만큼.
+        for y in range(y1, y2):                 # 직사각형의 세로길이만큼.
             board[x][y] = 1                     # 해당 좌표들을 색칠 (1로 변경)
 
 result = 0
