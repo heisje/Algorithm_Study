f155307590c156030019c9ee6490fba657411037
bbookng <bbookng@gmail.com>
2022-08-10 20:02:56 +0900

    [BOJ] 딱지놀이 / 브론즈1 / 124ms

--- bbookng/0810/14969_딱지놀이.py
+++ bbookng/0810/14969_딱지놀이.py
@@ -6,10 +6,10 @@ n = int(input())
 for k in range(n):
     arr1 = list(map(int, input().split()))
     arr2 = list(map(int, input().split()))
-    arr1.pop(0)
+    arr1.pop(0)                                 # 첫 번째 수는 카드의 갯수라 뺌
     arr2.pop(0)
 
-    x = 4
+    x = 4                                       # 별부터 시작하니까 4부터 시작
 
     for i in range(4):
         if arr1.count(x) > arr2.count(x):
@@ -18,9 +18,9 @@ for k in range(n):
         elif arr1.count(x) < arr2.count(x):
             print('B')
             break
-        else:
+        else:                                   # 똑같으면 다음 딱지로
             x -= 1
-        if x == 0:
-            print('D')
+        if x == 0:                              # 끝까지 무승부면
+            print('D')                          # 'D'
 
 
