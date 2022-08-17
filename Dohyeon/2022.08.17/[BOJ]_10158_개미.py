a2c4eff6f6c592826a67c1d1185e3de7c4e75915
michaia0363 <michaia0363@gmail.com>
2022-08-17 21:22:57 +0900

    [BOJ] 개미 / 실버 4 / 72ms 수정본

--- Dohyeon/2022.08.17/[BOJ]_10158_개미.py
+++ Dohyeon/2022.08.17/[BOJ]_10158_개미.py
@@ -1,57 +1,34 @@
 import sys
 input = lambda: sys.stdin.readline().strip()
 
-w, h = map(int, input().split())
-pos_x, pos_y = map(int, input().split())
+w, h = map(int, input().split())                # 돌아다닐 수 있는 판의 x, y
+pos_x, pos_y = map(int, input().split())        # 초기 위치
 
-t = int(input())
-
-direction = 1                               # 오른쪽 위 : 1, 왼쪽 위 : 2, 왼쪽 아래 : 3, 오른쪽 아래 : 4
-
-for move in range(t):
-
-    if direction == 1:
-        pos_x += 1
-        pos_y += 1
-        if pos_x == w and pos_y == h:
-            direction = 3                   # 꼭지점에 부딪히면 반대로 돌아간다
-        elif pos_x == w:
-            direction = 2                   # 오른쪽 변에 닿을 경우 왼쪽 위로
-        elif pos_y == h:
-            direction = 4                   # 천장에 닿을 경우 오른쪽 아래로
+t = int(input())                                # 시간
 
 
-    elif direction == 2:
-        pos_x -= 1
-        pos_y += 1
-        if pos_x == 0 and pos_y == h:       # 꼭지점에 부딪히면 반대로 돌아간다
-            direction = 4
-        elif pos_x == 0:                    # 왼쪽 변에 닿을 경우 오른쪽 위로
-            direction = 1
-        elif pos_y == h:                    # 천장에 닿을 경우 왼쪽 아래로
-            direction = 3
+if w - pos_x >= t:                              # 초기 x좌표 위치가 시간만큼 움직여도 벽에 닿지 않을 경우
+    pos_x = pos_x + t                           # 최종 x좌표 위치
+else:                                           # 벽에 닿을 만큼 시간이 넉넉한 경우
+    tx = t - (w - pos_x)                        # 벽에 닿았을 때의 남은 시간을 구한다
+    move_w = tx // w                            # 남은시간에 가로 길이를 나눈 몫을 통해 몇 번 닿는지 확인
+    x_d = tx % w                                # 나머지 값을 통해 위치를 특정
+    if move_w % 2 == 0:                         # 짝수번일 경우 오른쪽에서 나머지 만큼 떨어짐
+        pos_x = w - x_d
+    else:                                       # 홀수번일 경우 왼쪽에서 나머지 만큼 떨어짐
+        pos_x = x_d
+                                                # y좌표도 같은 방법으로 해결
+if h - pos_y >= t:
+    pos_y = pos_y + t
+else:
+    ty = t - (h - pos_y)
+    move_h = ty // h
+    y_d = ty % h
+    if move_h % 2 == 0:
+        pos_y = h - y_d
+    else:
+        pos_y = y_d
 
-
-    elif direction == 3:
-        pos_x -= 1
-        pos_y -= 1
-        if pos_x == 0 and pos_y == 0:       # 꼭지점에 부딪히면 반대로 돌아간다
-            direction = 1
-        elif pos_x == 0:                    # 왼쪽 변에 닿을 경우 오른쪽 아래로
-            direction = 4
-        elif pos_y == 0:                    # 바닥에 닿을 경우 왼쪽 위로
-            direction = 2
-
-    else:                                   # 오른쪽 아래 방향의 경우
-        pos_x += 1
-        pos_y -= 1
-        if pos_x == w and pos_y == 0:       # 꼭지점에 부딪히면 반대로 돌아간다
-            direction = 2
-        elif pos_x == w:                    # 오른쪽 변에 닿으면 왼쪽 아래로
-            direction = 3
-        elif pos_y == 0:                    # 바닥에 닿으면 오른쪽 위로
-            direction = 1
 print(pos_x, end=' ')
 print(pos_y)
-
 
