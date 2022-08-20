d34c039e8393edcb279cd1a32f5e90549f37e9e9
nimusmix <nimusmix@gmail.com>
2022-08-20 13:07:46 +0900

    [BOJ] 수열 / 실버3 / 120ms

---
+++ nimusmix/0824/2559.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+temp = list(map(int, input().split()))
+maxT = -9999999999999999999999
+sumT = 0
+
+for i in range(N-K+1):
+    if i == 0:
+        sumT = sum(temp[i:i+K])
+    else:
+        sumT = sumT - temp[i-1] + temp[i+K-1]
+        
+    if sumT > maxT:
+        maxT = sumT
+        
+print(maxT)
