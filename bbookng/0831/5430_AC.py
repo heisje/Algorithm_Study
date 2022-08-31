8dedb6feffb85bff71e8d7ef9760265c05fc61d5
bbookng <bbookng@gmail.com>
2022-08-31 20:44:06 +0900

    [BOJ] AC / 골드5 / 176ms

--- bbookng/0831/5430_AC.py
+++ bbookng/0831/5430_AC.py
@@ -1,6 +1,6 @@
 # import sys
 # from collections import deque
-# input = lambda : sys.stdin.readline().strip()
+# input = lambda: sys.stdin.readline().strip()
 #
 # T = int(input())
 # for tc in range(T):
@@ -26,38 +26,40 @@
 #
 #     if flag:
 #         if cnt % 2 == 0:
-#             print("[" + ",".join(arr) + "]")
+#             print('[' + ','.join(arr) + ']')
 #         else:
 #             arr.reverse()
-#             print("[" + ",".join(arr) + "]")
+#             print('[' + ','.join(arr) + ']')
 #     else:
 #         print('error')
 
 
 import sys
-input = lambda : sys.stdin.readline().strip()
+input = lambda: sys.stdin.readline().strip()
 
-opp = [1,0]
+opp = [1, 0]
 T = int(input())
 for _ in range(T):
     p = input()
     N = int(input())
     arr = list(input()[1:-1].split(','))
-    flag = 1
+    flag = 1                                        # 뒤집는걸 표시
     left = 0
-    right = len(arr) if not arr[0] == "" else 0
+    right = len(arr) if not arr[0] == "" else 0     # 빈 리스트를 받을 경우 에러처리를 위한 조건문
 
-    for i in p:
-        if i == "R":
-            flag = opp[flag]
-        else:
-            if flag:
+    for i in p:                                     # 수행할 함수
+        if i == "R":                                # R이면
+            flag = opp[flag]                        # flag로 뒤집힌 상태인지 아닌지 표시
+        else:                                       # D이면
+            if flag:                                # flag가 1일때 정방향 이므로 left를 +1
                 left += 1
-            else:
+            else:                                   # 0일때 역방향이므로 right를 +1
                 right -= 1
 
-    flag = -1 if not flag else 1
+    flag = -1 if not flag else 1                    # flag가 0일때 -1로 변경처리
+    # left가 right 보다 크면 리스트에 든게 없으므로 error
+    # left 부터 rigth 까지 result에 담고, flag에 따라서 배열 방향 결정
     result = arr[left:right][::flag] if left <= right else 'error'
-    if result != "error":
+    if result != 'error':
         result = '['+','.join(result)+']'
     print(result)
