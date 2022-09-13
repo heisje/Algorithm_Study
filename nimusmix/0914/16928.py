b1afcb9564e45f0f68cb0f54f03ecfef02494b5c
nimusmix <nimusmix@gmail.com>
2022-09-13 21:57:32 +0900

    [BOJ] 뱀과 사다리 게임 / 골드5 / 88ms

---
+++ nimusmix/0914/16928.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+def bfs():
+    queue = deque()
+    queue.append(1)
+    visited[1] = 0
+    
+    while queue:
+        s = queue.popleft()
+        
+        if s == 100:
+            return visited[100]
+        
+        for i in range(1, 7):
+            w = s+i
+            if adjList[w]: w = adjList[w][0]
+            if visited[w] == -1:
+                queue.append(w)
+                visited[w] = visited[s] + 1
+
+adjList = [[] for _ in range(107)]
+visited = [-1 for _ in range(107)]
+N, M = map(int, input().split())
+for _ in range(N+M):
+    a, b = map(int, input().split())
+    adjList[a].append(b)
+    
+print(bfs())
