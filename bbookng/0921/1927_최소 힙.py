d9621b04fc48f6e6ee15a9131760bdcb1331bb3d
bbookng <bbookng@gmail.com>
2022-09-21 20:32:06 +0900

    [BOJ] 최소힙 / 실버2 / 164ms

---
+++ bbookng/0921/1927_최소 힙.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+input = lambda :sys.stdin.readline().strip()
+
+N = int(input())
+heap = []
+
+for i in range(N):
+    X = int(input())
+    if X == 0:
+        print(heapq.heappop(heap)) if heap else print(0)
+    else:
+        heapq.heappush(heap, X)
+
+
+
+
