2d6b56587c9b99d292d883e6c8d41922a52204d1
EZ-000 <nongdam20@naver.com>
2022-09-22 01:24:20 +0900

    update

--- EZ-000/0914/BOJ_16928.py
+++ EZ-000/0914/BOJ_16928.py
@@ -35,3 +35,41 @@ while q:
                 visited[move] = 1                       # 방문 표시하고
                 cnt[move] = cnt[now] + 1                # 주사위 던진 횟수 저장하고
                 q.append(move)                          # 큐에 위치를 저장해!
+
+"""cnt 없이도 가능
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+
+N, M = map(int, input().split())
+ladders = {}
+snakes = {}
+for _ in range(N):
+    x, y = map(int, input().split())
+    ladders[x] = y
+for _ in range(M):
+    u, v = map(int, input().split())
+    snakes[u] = v
+
+q = deque([1])
+visited = [0] * 101
+
+while q:
+    now = q.popleft()
+    if now == 100:
+        print(visited[100])
+        break
+    for n in range(1, 7):
+        move = now + n
+        if move < 101 and not visited[move]:
+            if move in ladders.keys():
+                move = ladders[move]
+            if move in snakes.keys():
+                move = snakes[move]
+
+            if not visited[move]:
+                visited[move] = visited[now] + 1
+                q.append(move)
+"""
