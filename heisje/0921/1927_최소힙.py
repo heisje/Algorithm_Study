51858db13afab0fafe58efc13b236a4dcf1701cc
heeje <heeje1996@gmail.com>
2022-09-21 20:28:34 +0900

    [BOJ] 최소힙 / 실버2 / 152ms

---
+++ heisje/0921/1927_최소힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+N = int(input())
+
+a = []
+for _ in range(N):
+    value = int(sys.stdin.readline())
+    if value == 0:
+        if a:
+            print(heapq.heappop(a))
+        else:
+            print(0)
+    else:
+        heapq.heappush(a, value)
+
+#실버2 / 10분 / 152ms
