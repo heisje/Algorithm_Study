a347f0b7bb36ba905123b4b7a7d35c9025bca48a
bbookng <bbookng@gmail.com>
2022-09-21 20:50:44 +0900

    [BOJ] 테트로미노 / 골드4 / 4832ms

--- bbookng/0921/14500_테트로미노.py
+++ bbookng/0921/14500_테트로미노.py
@@ -7,41 +7,37 @@ N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 def solution(i, j, di, dj):
-    q = []
+    def make_candidates(array, tmp):
+        for _ in range(2):
+            value, ci, cj = heapq.heappop(array)
+            tmp -= value
+            visited.append((ci, cj))
 
+            for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                ni, nj = ci + di, cj + dj
+                if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in visited and (-arr[ni][nj], ni, nj) not in q:
+                    heapq.heappush(candidates, (-arr[ni][nj], ni, nj))
+        return tmp
+
+    q = []
     heapq.heappush(q, (-arr[i][j], i, j))
     heapq.heappush(q, (-arr[i+di][j+dj], i+di, j+dj))
+
     visited = []
     candidates = []
-    tmp = 0
 
-    for _ in range(2):
-        value, ci, cj = heapq.heappop(q)
-        tmp -= value
-        visited.append((ci, cj))
+    res = make_candidates(q, 0)
+    res = make_candidates(candidates, res)
 
-        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            ni, nj = ci + di, cj + dj
-            if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in visited and (-arr[ni][nj], ni, nj) not in q:
-                heapq.heappush(candidates, (-arr[ni][nj], ni, nj))
-
-    for _ in range(2):
-        value, ci, cj = heapq.heappop(candidates)
-        tmp -= value
-        visited.append((ci, cj))
-
-        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            ni, nj = ci + di, cj + dj
-            if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in visited and (-arr[ni][nj], ni, nj) not in q:
-                heapq.heappush(candidates, (-arr[ni][nj], ni, nj))
-
-    return tmp
+    return res
 
 answer = 0
 for i in range(N-1):
-    for j in range(M-1):
+    for j in range(M):
         answer = max(answer, solution(i, j, 1, 0))
+
+for i in range(N):
+    for j in range(M-1):
         answer = max(answer, solution(i, j, 0, 1))
 
 print(answer)
-
