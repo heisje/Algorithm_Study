ad18ee3958bbf77437aadab5def053c9d2e9c5b7
nimusmix <nimusmix@gmail.com>
2022-09-25 15:03:20 +0900

    [BOJ] 카잉 달력 / 실버1 / 1244ms

--- nimusmix/0928/6064.py
+++ nimusmix/0928/6064.py
@@ -1,7 +1,6 @@
 from math import lcm
 import sys
 input = lambda: sys.stdin.readline().strip()
-print = sys.stdout.write
 
 T = int(input())
 for tc in range(T):
@@ -9,7 +8,7 @@ for tc in range(T):
     LCM = lcm(M, N)
 
     for ans in range(x, LCM+1, M):
-        if (ans-x) % M == 0 and (ans-y) % N == 0:
+        if (ans-y) % N == 0:
             print(ans)
             break
     else: print(-1)
