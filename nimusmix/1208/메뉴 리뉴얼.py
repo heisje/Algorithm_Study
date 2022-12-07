a75e518821ec9d9f397efbca80b86484bbc0e4ba
nimusmix <nimusmix@gmail.com>
2022-12-08 02:06:40 +0900

    [PGS] 메뉴 리뉴얼 / Lv.2

--- nimusmix/1208/메뉴 리뉴얼.py
+++ nimusmix/1208/메뉴 리뉴얼.py
@@ -1,40 +1,51 @@
 from itertools import combinations
+from collections import defaultdict
 
 def solution(orders, course):
-    info = {}
     ans = []
-    
-    for order in orders:
-        for i in order:
-            info[i] = 1
+    all_combis = {}
 
     for num in course:
-        max_cnt = 0
-        tmp = []
-        
-        for combi in combinations(info, num):
-            cnt = 0
-            
-            for order in orders:
-                for i in combi:
-                    if i not in order:
-                        break
-                else:
-                    cnt += 1
+        all_combis[num] = defaultdict(int)
 
-            if cnt > 1:
-                max_cnt = max(max_cnt, cnt)
-                if max_cnt > 1 and max_cnt == cnt:
-                    tmp.append((cnt, ''.join(combi)))
-                    
-        for i in tmp:
-            if i[0] == max_cnt:
-                ans.append(i[1])
+    for order in orders:
+        for num in course:
+            for combi in combinations(order, num):
+                all_combis[num][tuple(sorted(combi))] += 1
+
+    for num in all_combis:
+        if not all_combis[num]:
+            continue
+        
+        max_cnt = max(all_combis[num].values())
+        if max_cnt < 2:
+            continue
+
+        for key, value in all_combis[num].items():
+            if value == max_cnt:
+                ans.append(''.join(key))
 
     ans.sort()
     return ans
 
 
-
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2, 3, 4]))
-# print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2, 3, 5]))
+# 테스트 1 〉	통과 (0.09ms, 10.3MB)
+# 테스트 2 〉	통과 (0.10ms, 10.4MB)
+# 테스트 3 〉	통과 (0.10ms, 10.1MB)
+# 테스트 4 〉	통과 (0.10ms, 10.3MB)
+# 테스트 5 〉	통과 (0.09ms, 10.3MB)
+# 테스트 6 〉	통과 (0.31ms, 10.3MB)
+# 테스트 7 〉	통과 (0.26ms, 10.2MB)
+# 테스트 8 〉	통과 (2.74ms, 10.3MB)
+# 테스트 9 〉	통과 (1.84ms, 10.4MB)
+# 테스트 10 〉	통과 (4.44ms, 10.7MB)
+# 테스트 11 〉	통과 (1.27ms, 10.4MB)
+# 테스트 12 〉	통과 (1.60ms, 10.4MB)
+# 테스트 13 〉	통과 (2.22ms, 10.4MB)
+# 테스트 14 〉	통과 (1.67ms, 10.5MB)
+# 테스트 15 〉	통과 (2.32ms, 10.5MB)
+# 테스트 16 〉	통과 (0.61ms, 10.4MB)
+# 테스트 17 〉	통과 (0.35ms, 10.3MB)
+# 테스트 18 〉	통과 (0.13ms, 10.4MB)
+# 테스트 19 〉	통과 (0.02ms, 10.2MB)
+# 테스트 20 〉	통과 (0.41ms, 10.2MB)
