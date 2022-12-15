8bf12c97b4bc2ae3290d5af0b4fbe5a0348754fb
bbookng <bbookng@gmail.com>
2022-12-15 21:00:33 +0900

    [PGS] 올바른 괄호 / Lv.2

---
+++ bbookng/1215/올바른 괄호.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+def solution(s):
+    q = deque()
+    for i in s:
+        if not q and i == ')':
+            return False
+        elif q and q[-1] == '(' and i == ')':
+            q.popleft()
+        else:
+            q.append(i)
+
+    return True if not q else False
+
+solution("()()")
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.00ms, 10.2MB)
+테스트 2 〉	통과 (0.00ms, 10.3MB)
+테스트 3 〉	통과 (0.00ms, 10.2MB)
+테스트 4 〉	통과 (0.00ms, 10.1MB)
+테스트 5 〉	통과 (0.00ms, 10.1MB)
+테스트 6 〉	통과 (0.00ms, 10.3MB)
+테스트 7 〉	통과 (0.01ms, 10.2MB)
+테스트 8 〉	통과 (0.01ms, 10.3MB)
+테스트 9 〉	통과 (0.01ms, 10.1MB)
+테스트 10 〉	통과 (0.00ms, 10.2MB)
+테스트 11 〉	통과 (0.01ms, 10.1MB)
+테스트 12 〉	통과 (0.01ms, 10.1MB)
+테스트 13 〉	통과 (0.01ms, 10.2MB)
+테스트 14 〉	통과 (0.01ms, 10.1MB)
+테스트 15 〉	통과 (0.02ms, 10.1MB)
+테스트 16 〉	통과 (0.01ms, 10.2MB)
+테스트 17 〉	통과 (0.02ms, 10.3MB)
+테스트 18 〉	통과 (0.01ms, 10.1MB)
+효율성  테스트
+테스트 1 〉	통과 (11.63ms, 10.3MB)
+테스트 2 〉	통과 (11.62ms, 10.5MB)
+'''
