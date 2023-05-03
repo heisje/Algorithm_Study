5a7f22a0b0e3c9dc128e64da62b2b8489069f99b
bbookng <bbookng@gmail.com>
2023-05-03 23:36:30 +0900

    [BOJ] 암기왕 / 실버4 / 1652ms

---
+++ bbookng/0504/2776_암기왕.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    N = int(input())
+    note1 = set(map(int, input().split()))
+    M = int(input())
+    note2 = list(map(int, input().split()))
+
+    for i in note2:
+        if i in note1:
+            print(1)
+        else:
+            print(0)
