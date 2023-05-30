29fb08324482d2c84589851804ad2708c4547e37
bbookng <bbookng@gmail.com>
2023-05-30 23:53:09 +0900

    [BOJ] 파이프 옮기기 1 / 골드5 / 44ms

--- bbookng/0601/17070_파이프 옮기기 1.py
+++ bbookng/0601/17070_파이프 옮기기 1.py
@@ -1,41 +1,16 @@
-from pprint import pprint
-
 N = int(input())
 
 board = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[[0, 0, 0] for _ in range(N+1)] for _ in range(N+1)]
-dp[0][0] = [1, 0, 0]
-dp[0][1] = [1, 0, 0]
+dp = [[[0, 0, 0] for _ in range(N)] for _ in range(N)]
+dp[0][1][0] = 1
 
-# 방향 고려
-# 벽 고려
 for i in range(N):
-    for j in range(1, N):
-        if board[i][j]:
-            dp[i][j] = [0, 0, 0]
-            continue
+    for j in range(2, N):
+        if board[i][j] == 0:
+            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2]
+            dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
+            if board[i-1][j] == 0 and board[i][j-1] == 0:
+                dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2]
 
-        # 가로
-        if dp[i][j][0] and j < N-1 and not board[i][j+1]:
-            dp[i][j+1][0] += dp[i][j][0]
-            dp[i + 1][j + 1][2] += dp[i][j][0]
-
-        # 세로
-        if dp[i][j][1] and i < N-1 and not board[i+1][j]:
-            dp[i+1][j][1] += dp[i][j][1]
-            dp[i + 1][j + 1][2] += dp[i][j][1]
-
-        # 대각선
-        if dp[i][j][2]:
-            if j < N-1 and not board[i][j+1]:
-                dp[i][j + 1][0] += dp[i][j][2]
-            if i < N-1 and not board[i+1][j]:
-                dp[i + 1][j][1] += dp[i][j][2]
-            if j < N-1 and i < N-1 and not board[i+1][j+1]:
-                dp[i + 1][j + 1][2] += dp[i][j][2]
-
-pprint(dp)
-print(dp[N-1][N-1])
-
-
+print(sum(dp[N-1][N-1]))
