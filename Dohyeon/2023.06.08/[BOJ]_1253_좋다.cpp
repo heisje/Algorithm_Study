52e788be97252d8f72c6f0da4cf733d2b7954354
michaia0363 <michaia0363@gmail.com>
2023-06-08 21:54:48 +0900

    [BOJ] 1253 좋다

---
+++ Dohyeon/2023.06.08/[BOJ]_1253_좋다.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int MAX_N = 2000;
+
+int arr[MAX_N];
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        int target = arr[i];
+        int left = 0, right = N - 1;
+
+        while (left < right) {
+            if (left == i) {
+                left++;
+                continue;
+            }
+            if (right == i) {
+                right--;
+                continue;
+            }
+
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                count++;
+                break;
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
+
+
+
