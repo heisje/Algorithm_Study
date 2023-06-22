770b9b9a5fa2341474a544b21c25a67d74cab2c7
michaia0363 <michaia0363@gmail.com>
2023-06-22 20:59:10 +0900

    BOJ / 골드 4 / 줄세우기

---
+++ Dohyeon/2023.06.22/[BOJ]_2631_줄세우기.cpp
@@ -0,0 +1,44 @@
+﻿// [BOJ]_2631_줄세우기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    // 데이터 입력
+    int n;
+    int answer;
+    cin >> n;
+
+    vector<int> children(n);
+    for (int i = 0; i < n; i++) {
+        cin >> children[i];
+    }
+
+
+    vector<int> dp(n, 1);       // n개를 1로 초기화
+
+    int max_length = 1;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {       // 앞에 있는 값들을 확인
+            if (children[j] < children[i]) {    // 순서가 맞다면
+                dp[i] = max(dp[i], dp[j] + 1);
+                max_length = max(max_length, dp[i]);
+            }
+        }
+    }
+
+
+    answer = n - max_length;                    // 움직여야 하는 횟수는 원래 길이 - 정렬된 값 수
+    cout << answer << endl;
+
+    return 0;
+
+}
+
