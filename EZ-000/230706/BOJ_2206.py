9a3b743607ca14967dc8c48fefe7c2afda3c5977
EZ-000 <nongdam20@naver.com>
2023-07-13 14:55:17 +0900

    [BOJ] 벽 부수고 이동하기 / 골드3 / 3996ms

---
+++ EZ-000/230706/BOJ_2206.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def bfs(arr):
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+    visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+    visited[0][0][0] = 1
+    que = deque([(0, 0, 0)])
+    while que:
+        row, col, flag = que.popleft()
+
+        if row == N - 1 and col == M - 1:
+            return visited[row][col][flag]
+
+        for idx in range(4):
+            nr, nc = row + dr[idx], col + dc[idx]
+            if -1 < nr < N and -1 < nc < M:
+                if not arr[nr][nc] and not visited[nr][nc][flag]:
+                    que.append((nr, nc, flag))
+                    visited[nr][nc][flag] = visited[row][col][flag] + 1
+                if arr[nr][nc] and not flag:
+                    que.append((nr, nc, 1))
+                    visited[nr][nc][1] = visited[row][col][0] + 1
+
+    return -1
+
+
+N, M = map(int, input().split())
+board = [list(map(int, input())) for _ in range(N)]
+
+walls = []
+for n in range(N):
+    for m in range(M):
+        if board[n][m]:
+            walls.append((n, m))
+
+print(bfs(board))
